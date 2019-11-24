d82ee701d1c801d7a963d8ba10a097baf494ea99
ZH-PYJ <emcicada@gmail.com>
2019-11-24 23:30:46 +0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -7,12 +7,17 @@ module.exports = (api, options) => {
       lint: "vue-cli-service lint"
     },
     dependencies: {
+      axios: "^0.19.0",
       "core-js": "^3.3.2",
-      vue: "^2.6.10"
+      vue: "^2.6.10",
+      "vue-router": "^3.1.3",
+      vuex: "^3.0.1"
     },
     devDependencies: {
       "@vue/cli-plugin-babel": "^4.0.0",
       "@vue/cli-plugin-eslint": "^4.0.0",
+      "@vue/cli-plugin-router": "^4.0.5",
+      "@vue/cli-plugin-vuex": "^4.0.5",
       "@vue/cli-service": "^4.0.0",
       "@vue/eslint-config-standard": "^4.0.0",
       "babel-eslint": "^10.0.3",
@@ -28,9 +33,7 @@ module.exports = (api, options) => {
       "*.{js,vue}": ["vue-cli-service lint", "git add"]
     }
   });
-  // files
+
+  // files(replace . with_ & _ with __)
   api.render("./template");
-  // 在generator模板裡template folder裡
-  // 檔案名稱開頭.要換成_
-  // 檔案名稱開頭_要換成__
 };
